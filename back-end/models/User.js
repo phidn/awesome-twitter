e53923753284c3de6143afb7cf573355b18ef543
Dang Nhat Phi <phidnuit@gmail.com>
2021-03-18 00:28:54 +0700

    jsonwebtoken & bcryptjs

--- back-end/models/User.js
+++ back-end/models/User.js
@@ -1,4 +1,5 @@
 const mongoose = require("mongoose");
+const bcrypt = require("bcryptjs");
 
 const userSchema = new mongoose.Schema({
   name: { type: String, unique: true, trim: true, 
@@ -12,6 +13,18 @@ const userSchema = new mongoose.Schema({
     minLength: [6, "Password must be at least 6 characters."]
   },
 }, {timestamps: true});
+
+userSchema.pre("save", function(next) {
+  let user = this;
+  bcrypt.hash(user.password, 10, function(error, hash) {
+    if(error) {
+      return next(error);
+    } else {
+      user.password = hash;
+      next();
+    }
+  });
+});
 
 const User = mongoose.model("User", userSchema);
 
